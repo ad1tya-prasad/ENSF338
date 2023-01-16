e8d94985024a06cfeab609076c07c92448f6b1df
AdityaP16 <ap.prasad@outlook.com>
2023-01-16 16:56:42 -0700

    records program

--- assign-1/q4/animals.py
+++ assign-1/q4/animals.py
@@ -1,16 +1,25 @@
 import json
 
-def add_unique_id(filepath):
+def average_above_5(filepath):
     # Load the data from the JSON file
     with open(filepath, 'r') as f:
         data = json.load(f)
 
-    # Add a unique ID field to each record
-    for i, record in enumerate(data):
-        record['unique_id'] = i
+    # initialize sum and count
+    sum = 0
+    count = 0
+    # iterate over the data
+    for record in data:
+        # if the value is above 5
+        if record['weight'] > 5:
+            sum += record['weight']
+            count += 1
+    # if there are records above 5
+    if count > 0:
+        avg = sum/count
+        print(avg)
+    else:
+        print("There are no records above 5")
 
-    # Save the data to a new file
-    with open('animals_modified.json', 'w') as f:
-        json.dump(data, f)
+average_above_5('assign-1/q4/animals.json')
 
-add_unique_id('assign-1/q4/animals.json')
