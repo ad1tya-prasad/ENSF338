04bce2944b25f7c3d266cdf709bf8f82ac24055f
AdityaP16 <ap.prasad@outlook.com>
2023-01-16 14:43:34 -0700

    change plot type

--- assign-1/q3/alphabet.py
+++ assign-1/q3/alphabet.py
@@ -19,14 +19,16 @@ def count_letters(file_path):
     return letter_freqs
 
 def plot_letter_freqs(letter_freqs):
+    # Plot the letter frequencies
     letters = sorted(letter_freqs.keys())
     freqs = [letter_freqs[letter] for letter in letters]
-    plt.bar(letters, freqs)
-    plt.xlabel('Letter')
-    plt.ylabel('Frequency')
+    plt.hist(freqs, bins=26, edgecolor='black')
+    plt.xlabel('Frequency')
+    plt.ylabel('Number of letters')
     plt.show()
 
 
+# Testing the code, can be used for any text file
 file_path = 'assign-1/q3/gutenberg.txt'
 letter_freqs = count_letters(file_path)
 print(letter_freqs)
