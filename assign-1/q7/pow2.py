10edac4a8d3d07dd0a4257f9b73d0b2543647b71
AdityaP16 <ap.prasad@outlook.com>
2023-01-17 22:06:35 -0700

    finish q8

--- assign-1/q7/pow2.py
+++ assign-1/q7/pow2.py
@@ -1,8 +1,29 @@
+
+import timeit
+
 def pow2(n):
     return 2**n
 
-   
+def pow2_for_loop(n):
+    result = []
+    for i in range(n + 1):
+        result.append(2**i)
+    return result
 
-if __name__ == "__main__":
-    n = int(input())
-    print(pow2(n))
+def pow2_list_comprehension(n):
+    return [2**i for i in range(n + 1)]
+
+
+# for loop function
+start = timeit.default_timer()
+for i in range(1000):
+    pow2_for_loop(1000)
+end = timeit.default_timer()
+print("Time taken for for loop function: ",end - start)
+
+# list comprehension function
+start = timeit.default_timer()
+for i in range(1000):
+    pow2_list_comprehension(1000)
+end = timeit.default_timer()
+print("Time taken for list comprehension function: ",end - start)
